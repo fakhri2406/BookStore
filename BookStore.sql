ae70768a02c6c06ef87d7eb53b5d4a6b19cd7de5
Гезалов Фахри Эмиль <Gezal_yv57@itstep.edu.az>
2024-12-13 17:17:37 +0400

    database created

---
+++ BookStore.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Users (
+    UserId INT PRIMARY KEY IDENTITY,
+    Username NVARCHAR(50) NOT NULL UNIQUE,
+    Password NVARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Books (
+    BookId INT PRIMARY KEY IDENTITY,
+    Title NVARCHAR(100) NOT NULL,
+    Author NVARCHAR(100) NOT NULL,
+    Publisher NVARCHAR(100) NOT NULL,
+    Pages INT NOT NULL,
+    Genre NVARCHAR(50) NOT NULL,
+    PublicationYear INT NOT NULL,
+    Cost DECIMAL(10, 2) NOT NULL,
+    SalePrice DECIMAL(10, 2) NOT NULL,
+    IsContinuation BIT NOT NULL,
+    ContinuationOf INT NULL,
+    FOREIGN KEY (ContinuationOf) REFERENCES Books(BookId)
+);
--- obj/Debug/net8.0-windows/Views/MainWindow.g.i.cs
+++ obj/Debug/net8.0-windows/Views/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace FinalADO.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/FinalADO;V1.0.0.0;component/views/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/FinalADO;component/views/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
