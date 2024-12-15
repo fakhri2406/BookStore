05516ee6f09b12112d32d9e5813712bb2299dd45
Гезалов Фахри Эмиль <Gezal_yv57@itstep.edu.az>
2024-12-15 21:07:17 +0400

    auto changes

--- obj/Debug/net8.0-windows/Views/LoginWindow.g.i.cs
+++ obj/Debug/net8.0-windows/Views/LoginWindow.g.i.cs
@@ -69,7 +69,7 @@ namespace FinalADO.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/FinalADO;V1.0.0.0;component/views/loginwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/FinalADO;component/views/loginwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\LoginWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
